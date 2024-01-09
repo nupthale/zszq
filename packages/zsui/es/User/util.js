d4390050c94741095af2fcca21f95dafbb0edaf6
nupthale <nupthale@163.com>
2024-01-09 12:52:00 +0800

    fix: textButton增加disabled样式

--- packages/zsui/es/User/util.js
+++ packages/zsui/es/User/util.js
@@ -11,34 +11,7 @@ function hex2rgb(hex) {
     parseInt(color[2], 16)
   ].join(",");
 }
-const colors = [
-  "#1e80ff",
-  "#f53f3f",
-  "#0fbf60",
-  "#ff8b07",
-  "#f54a45",
-  "#10C3A9",
-  "#1c4cba",
-  "#3370ff",
-  "#078372",
-  "#00d6b9",
-  "#237b19",
-  "#34c724",
-  "#667901",
-  "#b3d600",
-  "#aa7803",
-  "#ffc60a",
-  "#b26206",
-  "#ff8800",
-  "#ac2f28",
-  "#f54a45",
-  "#9e1361",
-  "#f01d94",
-  "#161fa2",
-  "#4954e6",
-  "#8c218c",
-  "#d136d1"
-];
+const colors = ["#1e80ff", "#0fbf60", "#ff8b07", "#10C3A9"];
 const nameToColor = (name) => {
   const index = parseInt(String(name.charCodeAt(name.length - 1)), 10) % colors.length;
   const hexColor = colors[index];
